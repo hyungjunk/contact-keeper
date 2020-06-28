16142411c7e072c86adbb86fe97ebfd929cd2db6
hyungjunk <khjgd2@gmail.com>
2020-06-28 22:02:21 +0900

    init jwt tutorial

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+
+// Connect DB
+connectDB();
+
+// Init middleware - 이것을 사용함으로써 req.body가 그냥 JSON 받듯이 받아진다.
+app.use(express.json({ extended: false }));
+
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
@@ -7,7 +15,6 @@ app.get('/', (req, res) => {
 });
 
 // Define Routes
-
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
